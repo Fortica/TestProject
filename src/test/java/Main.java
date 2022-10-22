c498ac134a468c00b1048c02476ad21504c810a4
Mercurius Sphere <gayagaga0404@gmail.com>
2022-10-22 16:52:06 +0300

    Create Main class

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/larisa/Documents/INTELLIJ/WebDriver/bin/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://qa-mesto.praktikum-services.ru/");
+        driver.findElements(By.id("email"));
+
+        Thread.sleep(3000L);
+        driver.quit();
+    }
+
 }
